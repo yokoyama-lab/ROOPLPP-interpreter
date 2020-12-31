f62d9fe8b0884cfd88d5b331c4b43d3cc06c561c
iori12 <ioritopurin@gmail.com>
2021-01-01 08:11:24 +0900

    main.mlの不要な部分を消去

--- src/main.ml
+++ src/main.ml
@@ -43,23 +43,7 @@ let () =
        end
   | _ -> failwith "not implemented"
 
-(*let () =
-  let file_name = Printf.sprintf "%s" (Sys.argv.(1)) in
-  let channel = open_in file_name in
-  try let prog = parse channel in
-      let _ = close_in channel in
-      eval prog
-  with Util.Parse_error (start_pos, end_pos) ->
-    Printf.printf "Parse error at %d.%d-%d.%d\n"
-      start_pos.pos_lnum (start_pos.pos_cnum - start_pos.pos_bol)
-      end_pos.pos_lnum (end_pos.pos_cnum - end_pos.pos_bol);
-    exit 1
-
-
-open Syntax
-open Print
-open Eval
-
+(*
 let parse str =
   Parser.main Lexer.token
     (Lexing.from_string str)
